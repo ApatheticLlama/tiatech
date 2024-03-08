59b59a34b6f58cf97c22ef212d2afdcfc3e0d5d1
purebluez <notpurebluez@gmail.com>
2024-03-07 21:11:12 -0600

    add to mod file and multiblock system

--- src/main/java/tiatech/TiaTech.java
+++ src/main/java/tiatech/TiaTech.java
@@ -1,2 +1,32 @@
-package tiatech;public class TiaTech {
+package tiatech;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.util.ResourceLocation;
+
+import gregtech.GTInternalTags;
+
+import tiatech.api.utils.TTLog;
+import tiatech.common.metatileentities.TiaTechMetaTileEntities;
+
+import org.jetbrains.annotations.NotNull;
+
+@Mod(modid = TiaTech.MODID,
+        name = TiaTech.NAME,
+        version = TiaTech.VERSION,
+        dependencies = GTInternalTags.DEP_VERSION_STRING)
+public class TiaTech {
+
+    public static final String MODID = "tiatech";
+    public static final String NAME = "TiaTech";
+    public static final String VERSION = "0.0.1";
+
+    public static @NotNull ResourceLocation ID(@NotNull String path) {
+        return new ResourceLocation(TiaTech.MODID, path);
+    }
+    @Mod.EventHandler
+    public void onPreInit(@NotNull FMLPreInitializationEvent event) {
+        TTLog.init(event.getModLog());
+        TiaTechMetaTileEntities.init();
+    }
 }
